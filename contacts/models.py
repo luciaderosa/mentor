419472d69b027f79d4c5dd6585e71c4e152f0cdf
luciaderosa <luciaderosa@hotmail.com>
2022-10-31 14:36:58 -0300

    contact form ok / falta validación repetido

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name= models.CharField(max_length=100)
+    email=models.EmailField(max_length=254) #RFC 2821
+    subject=models.CharField(max_length=60)
+    message=models.CharField(max_length=512) #Postgres accepts VARCHAR max of 655536
+    isAnswered=models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name +' - '+self.subject
