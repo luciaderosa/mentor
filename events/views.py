4f1bf62dfaff5414acdd779e176c0a94b0534faa
Rodrigo Lascano <rodrigo.lascano@gmail.com>
2022-11-12 18:35:24 -0300

    Se agregaron mensajes a crud Events

--- events/views.py
+++ events/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 
 from .models import Event
@@ -10,15 +11,18 @@ from .forms import EventForm
 class EventListView(ListView):    
     model = Event
 
-class EventCreateView(CreateView):
+class EventCreateView(SuccessMessageMixin, CreateView):
     model = Event
     form_class = EventForm
+    success_message = 'Event was created successfully'
 
-class EventUpdateView(UpdateView):
+class EventUpdateView(SuccessMessageMixin, UpdateView):
     model = Event
     form_class = EventForm
     template_name_suffix = '_update_form'    
+    success_message = 'Event was updated successfully'
 
-class EventDeleteView(DeleteView):
+class EventDeleteView(SuccessMessageMixin, DeleteView):
     model = Event
     success_url = reverse_lazy('events')
+    success_message = "Event deleted"
